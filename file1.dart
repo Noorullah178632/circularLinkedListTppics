df00c7bffbf684c4699f273abaf85c280e80ce14
NoorUllah <nooullah178632@gmail.com>
2025-10-06 23:42:20 +0500

    new data

--- file1.dart
+++ file1.dart
@@ -10,8 +10,9 @@ class CircularLinkedList {
   void addAtBeginnig(int data) {
     Node newNode = Node(data);
     if (head == null) {
-      newNode.next = head;
       head = newNode;
+      newNode.next = head;
+
       return;
     }
     Node temp = head!;
@@ -41,6 +42,101 @@ class CircularLinkedList {
     } while (temp != head);
     print('Value :$searchValue not found in the search');
   }
+
+  //add value it lat
+  void addAtLast(int data) {
+    Node newNode = Node(data);
+    if (head == null) {
+      head = newNode;
+      newNode.next = head;
+
+      return;
+    }
+    Node temp = head!;
+    while (temp.next != head) {
+      temp = temp.next!;
+    }
+    temp.next = newNode;
+    newNode.next = head;
+  }
+
+  //now i will do some function for the deletion
+  void deleteAtFirst() {
+    if (head == null) {
+      print("List is empty");
+      return;
+    }
+    if (head!.next == head) {
+      head = null;
+      return;
+    }
+    Node temp = head!;
+    while (temp.next != head) {
+      temp = temp.next!;
+    }
+    temp.next = head!.next;
+    head = head!.next;
+  }
+
+  // //remove a value from the circular linked list
+  // void removeValue(int data) {
+  //   if (head == null) {
+  //     return print("The list is empty ");
+  //   }
+
+  //   Node temp = head!;
+  //   while (temp.next != head) {
+  //     if (temp.data == data) {
+  //       newNode = temp.next!;
+
+  //       return;
+  //     }
+  //     temp = temp.next!;
+  //   }
+  //   print("No value :$data present in the circular linked list");
+  // }
+
+  //delete last node
+  void deleteAtLast() {
+    if (head == null) {
+      return print("List is empty");
+    }
+    if (head!.next == head) {
+      head = null;
+      return;
+    }
+    Node temp = head!;
+    while (temp.next!.next != head) {
+      temp = temp.next!;
+    }
+
+    Node lastNode = temp.next!;
+    temp.next = head;
+  }
+
+  //showing the linked list
+  List showLinkedList() {
+    Node temp = head!;
+    List<int> showList = [];
+
+    do {
+      showList.add(temp.data!);
+      temp = temp.next!;
+    } while (temp != head);
+    return showList;
+  }
 }
 
-void main() {}
+void main() {
+  CircularLinkedList a = CircularLinkedList();
+  a.addAtBeginnig(40);
+  a.addAtBeginnig(4);
+  a.addAtBeginnig(20);
+  a.addAtBeginnig(50);
+  a.addAtBeginnig(210);
+  a.addValueAfterValue(50, 10);
+  a.addAtLast(90);
+  a.deleteAtFirst();
+  a.deleteAtLast();
+  print(a.showLinkedList());
+}
